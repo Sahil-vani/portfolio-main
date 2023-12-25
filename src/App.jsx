2a5fe761577eebf14684670244af714c73ea6770
Sahil Vani <sahilvani007@gmail.com>
2023-12-25 12:01:32 +0530

    updating app.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import HeroPage from "./components/heroPage/HeroPage";
 import AboutPage from "./components/aboutPage/AboutPage";
 import ProjectPage from "./components/projectPage/ProjectPage";
 import Footer from "./components/footer/Footer";
+import "./app.scss";
 
 function App() {
   return (
